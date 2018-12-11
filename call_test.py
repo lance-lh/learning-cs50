db7ab1bec12f236471aa1895692366a4ea1ee5b8
haolee <imleehao@gmail.com>
2018-12-11 20:57:04 +0800

    add two test files

---
+++ call_test.py
@@ -0,0 +1,5 @@
+import test
+
+print(__name__)
+print(test.__name__)
+
---
+++ test.py
@@ -0,0 +1,12 @@
+foo = 1
+
+def functionA():
+    print("Function A")
+
+def functionB():
+    print("Function B")
+
+if __name__ == '__main__':
+    functionA()
+    functionB()
+    print(foo)
